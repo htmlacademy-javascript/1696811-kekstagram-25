0638792f31ee8cf2be624f8e2d1f9276a91f0449
Keks <keks@htmlacademy.ru>
2022-03-15 19:39:05 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/fragment.js
@@ -0,0 +1,19 @@
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+function getPicture (picture) {
+  const template = pictureTemplate.cloneNode(true);
+  template.querySelector('.picture__img').src = picture.url;
+  template.querySelector('.picture__likes').textContent = picture.likes;
+  template.querySelector('.picture__comments').textContent = picture.comments.length;
+  return template;
+}
+
+export function getFragment (pictures) {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    fragment.append(getPicture(picture));
+  });
+  return fragment;
+}
