10a9f14bffeccb3c35904d0ad1402f5884bfddee
Keks <keks@htmlacademy.ru>
2022-03-16 21:22:13 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/fragment.js
@@ -0,0 +1,22 @@
+import { openGalery } from './galery.js';
+
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+function getPicture (picture) {
+  const template = pictureTemplate.cloneNode(true);
+  template.querySelector('.picture__img').src = picture.url;
+  template.querySelector('.picture__likes').textContent = picture.likes;
+  template.querySelector('.picture__comments').textContent = picture.comments.length;
+  template.addEventListener('click', () => openGalery(picture));
+  return template;
+}
+
+export function getFragment (pictures) {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    fragment.append(getPicture(picture));
+  });
+  return fragment;
+}
